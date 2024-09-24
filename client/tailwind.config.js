99ceab58748fd0dc39fc199b2676376ca87dbf59
techdivines <129510899+techdivines@users.noreply.github.com>
2024-09-24 16:11:51 +0530

    css setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: [],
   theme: {
   	extend: {
   		borderRadius: {
@@ -54,5 +53,9 @@ export default {
   	}
   },
   plugins: [require("tailwindcss-animate")],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
 }
 
